771e49f153613e2b1f6ddec3495eb69e87acda68
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2023-08-04 13:37:15 +0200

    comunica runner support different mode

--- comunica_runner.mjs
+++ comunica_runner.mjs
@@ -1,8 +1,8 @@
+import { QueryEngineFactory as QueryEngineFactoryLTQT} from "@comunica/query-sparql-link-traversal";
 import { QueryEngine } from "@comunica/query-sparql-link-traversal";
+
 import fs from 'fs';
 import { Command } from 'commander';
-import * as https from 'node:https';
-import * as http from 'node:http';
 
 const program = new Command();
 program
@@ -11,7 +11,7 @@ program
     .version('0.0.0')
 
     .requiredOption('-f, --file-path <string>', 'File path of the query to be executed')
-    .option('-l, --LTQT <boolean>', 'if true execute an LTQT query', true)
+    .option('-m, --mode <mode>', 'if true execute an LTQT query', 'TREE')
     .requiredOption('-r, --root-nodes <string...>', 'the first data sources to query')
     .option('-t, --timeout <number>', 'Timeout of the query in second', 120)
     .parse(process.argv);
@@ -20,10 +20,23 @@ const options = program.opts();
 const queryFilePath = options.filePath;
 const timeout = options.timeout * 1000;
 const rootNodes = options.rootNodes;
+const mode = options.mode;
+let engine = null;
+
+if (mode === "TREE") {
+    const config = "./evaluation/config_comunica_follow_tree.json";
+    engine = await new QueryEngineFactoryLTQT().create({ configPath: config });
+} else if (mode === "TREE-GUIDED") {
+    const config = "./evaluation/config_comunica_follow_tree_solver.json";
+    engine = await new QueryEngineFactoryLTQT().create({ configPath: config });
+} else if (mode === "DATA-DUMP") {
+    engine = new QueryEngine();
+} else {
+    throw new Error(`the mode ${mode} is not supported`);
+}
 
 const regexpSummary = /(TOTAL),([+-]?[0-9]*[.]?[0-9]+),([0-9]+)/;
 
-console.log(rootNodes);
 const returnedMessage = (nResults, timeExec, nRequest) => {
     const resp = {
         nResults,
@@ -34,9 +47,7 @@ const returnedMessage = (nResults, timeExec, nRequest) => {
 };
 
 const query = fs.readFileSync(queryFilePath).toString();
-console.log(query);
 
-const engine = await new QueryEngine();
 const results = await engine.query(
     query, {
     sources: rootNodes,
@@ -58,7 +69,6 @@ data.on('data', (res) => {
         nRequests = Number(tag[3])
     }
     nResults += 1;
-    console.log(String(res));
 });
 
 data.on('error', (_) => {
@@ -67,15 +77,12 @@ data.on('error', (_) => {
 });
 
 data.on('end', () => {
-    const endTime = new Date();
 
     console.log(returnedMessage(nResults, timeExec, nRequests));
     process.exit(0)
 });
 
 setTimeout(() => {
-    const endTime = new Date();
-
     console.log(returnedMessage(nResults, timeout));
     process.exit(1);
 }, timeout);
